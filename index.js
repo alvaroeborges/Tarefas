db30def3383279c653a5301385f8e6520f7f9606
alvaroeborges <alvaro.eborges@gmail.com>
2025-08-25 19:15:11 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const form = document.querySelector(".form");
+const input = document.querySelector(".input");
+const list = document.querySelector(".list");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const task = input.value.trim();
+  if (task) {
+    addTask(task);
+    input.value = "";
+  }
+});
+
+function addTask(task) {
+  const li = document.createElement("li");
+  li.innerHTML = `
+    ${task} <i class="fas fa-check-square"></i><i class="fas fa-trash"></i>
+  `;
+  list.appendChild(li);
+}
+list.addEventListener("click", function (e) {
+  if (e.target.classList.contains("fa-trash")) {
+    e.target.parentElement.remove();
+  }
+  if (e.target.classList.contains("fa-check-square")) {
+    e.target.parentElement.classList.toggle("checked");
+  }
+});
+
